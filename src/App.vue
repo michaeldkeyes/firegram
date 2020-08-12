6abdec62c55a5c7e7c1bb8ae330aa932790fdb0d
Michael Keyes <michael.d.keyes87@gmail.com>
2020-08-12 14:24:44 -0400

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Title />
+  </div>
+</template>
+
+<script>
+import Title from "./components/Title.vue";
+
+export default {
+  name: "App",
+  components: {
+    Title,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "_config.scss";
+
+body {
+  font-family: "Noto Serif";
+  color: $secondary;
+}
+
+#app {
+  max-width: 960px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="title">
+    <h1>FireGram</h1>
+    <h2>Your Pictures</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/_config.scss";
+
+.title {
+  h1 {
+    color: $primary;
+    font-size: 1.2rem;
+    letter-spacing: 2px;
+    font-weight: normal;
+  }
+
+  h2,
+  p {
+    text-align: center;
+  }
+
+  h2 {
+    margin-top: 60px;
+    font-size: 2.6rem;
+  }
+}
+</style>
